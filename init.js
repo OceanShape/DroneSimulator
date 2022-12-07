c3924f3696caacaf3d64f53a2a13c573dc96275b
OceanShape <oceanshape700@gmail.com>
2022-12-07 15:39:27 +0900

    refactor: Change file structure

--- init.js
+++ init.js
@@ -1,4 +1,8 @@
-﻿// 엔진 로드 후 실행할 초기화 함수(Module.postRun)
+﻿var GLOBAL = {
+	engineDirectory : "./engine/"
+}
+
+// 엔진 로드 후 실행할 초기화 함수(Module.postRun)
 function init() {
 
 	// 엔진 초기화 API 호출(필수)
@@ -38,7 +42,7 @@ var Module = {
 ;(function(){   	
 
 	// 1. XDWorldEM.asm.js 파일 로드
-	var file = "./js/XDWorldEM.asm.js";
+	var file = GLOBAL.engineDirectory + "XDWorldEM.asm.js";
 	
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', file, true);
@@ -51,14 +55,14 @@ var Module = {
 		// 2. XDWorldEM.html.mem 파일 로드
 		setTimeout(function() {
 			(function() {
-				var memoryInitializer = "./js/XDWorldEM.html.mem";
+				var memoryInitializer = GLOBAL.engineDirectory + "XDWorldEM.html.mem";
 				var xhr = Module['memoryInitializerRequest'] = new XMLHttpRequest();
         		xhr.open('GET', memoryInitializer, true);
 					xhr.responseType = 'arraybuffer';
 					xhr.onload =  function(){
 						
 						// 3. XDWorldEM.js 파일 로드
-						var url = "./js/XDWorldEM.js";
+						var url = GLOBAL.engineDirectory + "XDWorldEM.js";
 						var xhr = new XMLHttpRequest();
 						xhr.open('GET',url , true);
 						xhr.onload = function(){
