3b2272b7e85821e953b3005c85aeaabe71ada3c6
OceanShape <oceanshape700@gmail.com>
2022-12-19 10:39:29 +0900

    refactor: Move initialize functions to init.js

--- function.js
+++ function.js
@@ -5,26 +5,6 @@ var GLOBAL = {
 	TPFilePath : "./menu-select-mode.html"
 }
 
-// 엔진 로드 후 실행할 초기화 함수(Module.postRun)
-function init() {
-
-	//document.getElementById("data").innerHTML="./fp.html";
-
-	Module.Start(window.innerWidth, window.innerHeight);
-
-	var camera = Module.getViewCamera();
-
-	Module.XDEMapCreateLayer("facility_build", "https://xdworld.vworld.kr", 0, true, true, false, 9, 0, 15);
-
-	GLOBAL.canvas = Module.canvas
-
-	GLOBAL.currentFilePath = GLOBAL.TPFilePath
-
-	camera.setLocation(new Module.JSVector3D(126.91534283205316, 37.53060216016567, 836.298700842075));
-
-	includeHTML()
-}
-
 function setMove() {
 	let camera = Module.getViewCamera();
 	let lon, lat, alt;
@@ -40,10 +20,6 @@ function setMove() {
 	let pos = new Module.JSVector3D(lon, lat, alt);
 	camera.setLocation(pos);
 	camera.setTilt()
-}
-
-function clearPOI() {
-	
 }
 
 function changeMode() {
@@ -75,66 +51,3 @@ function initEvent(_canvas) {
 		console.log("MOUSE event");
 	});
 }
-
-// HTML 인클루드
-function includeHTML() {
-	var includeElements = document.getElementsByClassName("include-html")
-	for (var element of includeElements) {
-		var xhttp;
-		var file = GLOBAL.currentFilePath
-		if (file) {
-			xhttp = new XMLHttpRequest();
-			xhttp.onreadystatechange = function() {
-				if (this.readyState == 4) {
-					if (this.status == 200) {element.innerHTML = this.responseText;}
-					if (this.status == 404) {element.innerHTML = "Page not found.";}
-				}
-			}
-			xhttp.open("GET", file, true);
-			xhttp.send();
-			return;
-		}
-	}
-}
-
-// 엔진 파일 로드
-(function(){
-
-	// 1. XDWorldEM.asm.js 파일 로드
-	var file = GLOBAL.engineDirectory + "XDWorldEM.asm.js";
-	
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', file, true);
-	xhr.onload = function() {
-	
-		var script = document.createElement('script');
-		script.innerHTML = xhr.responseText;
-		document.body.appendChild(script);
-		
-		// 2. XDWorldEM.html.mem 파일 로드
-		setTimeout(function() {
-			(function() {
-				var memoryInitializer = GLOBAL.engineDirectory + "XDWorldEM.html.mem";
-				var xhr = Module['memoryInitializerRequest'] = new XMLHttpRequest();
-        		xhr.open('GET', memoryInitializer, true);
-					xhr.responseType = 'arraybuffer';
-					xhr.onload =  function(){
-						
-						// 3. XDWorldEM.js 파일 로드
-						var url = GLOBAL.engineDirectory + "XDWorldEM.js";
-						var xhr = new XMLHttpRequest();
-						xhr.open('GET',url , true);
-						xhr.onload = function(){
-							var script = document.createElement('script');
-							script.innerHTML = xhr.responseText;
-							document.body.appendChild(script);
-						};
-						xhr.send(null);
-					}
-					xhr.send(null);
-				})();
-			}, 1);
-		};
-		xhr.send(null);
-	}
-)();
--- init.js
+++ init.js
@@ -1,4 +1,83 @@
-﻿var Module = {
+﻿function init() {
+	Module.Start(window.innerWidth, window.innerHeight);
+
+	var camera = Module.getViewCamera();
+
+	Module.XDEMapCreateLayer("facility_build", "https://xdworld.vworld.kr", 0, true, true, false, 9, 0, 15);
+
+	GLOBAL.canvas = Module.canvas
+
+	GLOBAL.currentFilePath = GLOBAL.TPFilePath
+
+	camera.setLocation(new Module.JSVector3D(126.91534283205316, 37.53060216016567, 836.298700842075));
+
+	includeHTML()
+}
+
+// HTML include
+function includeHTML() {
+	var includeElements = document.getElementsByClassName("include-html")
+	for (var element of includeElements) {
+		var xhttp;
+		var file = GLOBAL.currentFilePath
+		if (file) {
+			xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function() {
+				if (this.readyState == 4) {
+					if (this.status == 200) {element.innerHTML = this.responseText;}
+					if (this.status == 404) {element.innerHTML = "Page not found.";}
+				}
+			}
+			xhttp.open("GET", file, true);
+			xhttp.send();
+			return;
+		}
+	}
+}
+
+// 엔진 파일 로드
+(function(){
+
+	// 1. XDWorldEM.asm.js 파일 로드
+	var file = GLOBAL.engineDirectory + "XDWorldEM.asm.js";
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', file, true);
+	xhr.onload = function() {
+	
+		var script = document.createElement('script');
+		script.innerHTML = xhr.responseText;
+		document.body.appendChild(script);
+		
+		// 2. XDWorldEM.html.mem 파일 로드
+		setTimeout(function() {
+			(function() {
+				var memoryInitializer = GLOBAL.engineDirectory + "XDWorldEM.html.mem";
+				var xhr = Module['memoryInitializerRequest'] = new XMLHttpRequest();
+        		xhr.open('GET', memoryInitializer, true);
+					xhr.responseType = 'arraybuffer';
+					xhr.onload =  function(){
+						
+						// 3. XDWorldEM.js 파일 로드
+						var url = GLOBAL.engineDirectory + "XDWorldEM.js";
+						var xhr = new XMLHttpRequest();
+						xhr.open('GET',url , true);
+						xhr.onload = function(){
+							var script = document.createElement('script');
+							script.innerHTML = xhr.responseText;
+							document.body.appendChild(script);
+						};
+						xhr.send(null);
+					}
+					xhr.send(null);
+				})();
+			}, 1);
+		};
+		xhr.send(null);
+	}
+)();
+
+var Module = {
 	TOTAL_MEMORY: 256*1024*1024,
 	postRun: [init],
 	canvas: (function() {
