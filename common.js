cad10983b74d8a8189f933b0533d9926ded5d14c
OceanShape <oceanshape700@gmail.com>
2022-12-22 08:39:35 +0900

    refactor: Create callback functions for addEvent()

--- common.js
+++ common.js
@@ -12,79 +12,55 @@ function changeMode() {
 	includeHTML()
 }
 
+function keyPressCallback(event) {
+	const delta = 0.0001
+	pos = Module.getViewCamera().getLocation()
+
+	if (event.key === 'w' || event.key === 'W') {
+		pos.Longitude += delta
+	} else if (event.key === 'x' || event.key === 'X') {
+		pos.Longitude -= delta
+	}
+
+	if (event.key === 'a' || event.key === 'A') {
+		pos.Latitude += delta
+	} else if (event.key === 'd' || event.key === 'D') {
+		pos.Latitude -= delta
+	}
+
+	if (event.key === 'c' || event.key === 'C') {
+		pos.Altitude += 10
+	} else if (event.key === 'z' || event.key === 'Z') {
+		pos.Altitude -= 10
+	}
+
+	Module.getViewCamera().moveLonLatAlt(pos.Longitude, pos.Latitude, pos.Altitude, true)
+	printPosition(pos)
+}
+
+function wheelCallback() {
+  printCamera(camera);
+}
+
+function mouseMoveCallback() {
+  printCamera(camera);
+}
+
 /* 마우스 & 키보드 이벤트 설정 */
 function addEvent() {
-	window.addEventListener("keypress", function(e) {
-		const delta = 0.0001
-		pos = Module.getViewCamera().getLocation()
+	window.addEventListener("keypress", keyPressCallback);
 
-		if (e.key === 'w' || e.key === 'W') {
-			pos.Longitude += delta
-		} else if (e.key === 'x' || e.key === 'X') {
-			pos.Longitude -= delta
-		}
+	Module.canvas.addEventListener('mousemove', mouseMoveCallback);
 
-		if (e.key === 'a' || e.key === 'A') {
-			pos.Latitude += delta
-		} else if (e.key === 'd' || e.key === 'D') {
-			pos.Latitude -= delta
-		}
-
-		if (e.key === 'c' || e.key === 'C') {
-			pos.Altitude += 10
-		} else if (e.key === 'z' || e.key === 'Z') {
-			pos.Altitude -= 10
-		}
-
-		Module.getViewCamera().moveLonLatAlt(pos.Longitude, pos.Latitude, pos.Altitude, true)
-		printPosition(pos)
-	});
-
-	Module.canvas.addEventListener('mousemove', function(e) {
-		let camera = Module.getViewCamera()
-		printCamera(camera)
-	});
-
-	Module.canvas.addEventListener('wheel', function(e) {
-		printCamera()
-	});
+	Module.canvas.addEventListener('wheel', wheelCallback);
 }
 
 function removeEvent() {
-	window.addEventListener("keypress", function(e) {
-		const delta = 0.0001
-		pos = Module.getViewCamera().getLocation()
+	window.removeEventListener("keypress", keyPressCallback);
 
-		if (e.key === 'w' || e.key === 'W') {
-			pos.Longitude += delta
-		} else if (e.key === 'x' || e.key === 'X') {
-			pos.Longitude -= delta
-		}
+	Module.canvas.removeEventListener('mousemove', mouseMoveCallback);
 
-		if (e.key === 'a' || e.key === 'A') {
-			pos.Latitude += delta
-		} else if (e.key === 'd' || e.key === 'D') {
-			pos.Latitude -= delta
-		}
-
-		if (e.key === 'c' || e.key === 'C') {
-			pos.Altitude += 10
-		} else if (e.key === 'z' || e.key === 'Z') {
-			pos.Altitude -= 10
-		}
-
-		Module.getViewCamera().moveLonLatAlt(pos.Longitude, pos.Latitude, pos.Altitude, true)
-		printPosition(pos)
-	});
-
-	Module.canvas.addEventListener('mousemove', function(e) {
-		let camera = Module.getViewCamera()
-		printCamera(camera)
-	});
-
-	Module.canvas.addEventListener('wheel', function(e) {
-		printCamera()
-	});
+	Module.canvas.removeEventListener('wheel', wheelCallback);
 }
 
 function setItemValue(_div, _value) {
