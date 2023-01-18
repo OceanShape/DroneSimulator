2261eaa7eeadd73e824920a755a63172213b34a1
OceanShape <oceanshape700@gmail.com>
2023-01-18 15:11:22 +0900

    design: Resize arrow

--- driving.js
+++ driving.js
@@ -84,9 +84,9 @@ function drawArrow(target) {
         0.0,
         90.0,
         10,
-        5,
-        0.1,
-        3.0,
+        1,
+        0.2,
+        2.0,
         new Module.JSColor(255, 255, 0, 0)
     );
 
